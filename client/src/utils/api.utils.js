6888d5158ea69a4815d0863872507ffc801c1ca7
dkswami <dkswami1820@gmail.com>
2023-01-17 00:49:04 +0530

    user bug resolved

--- client/src/utils/api.utils.js
+++ client/src/utils/api.utils.js
@@ -1,6 +1,6 @@
 export const backendURL = process.env.REACT_APP_BACKEND_URL;
 export const registerUrl = `${backendURL}/api/v1/auth/register`;
 export const loginUrl = `${backendURL}/api/v1/auth/login`;
-export const getAllUsersUrl = `${backendURL}/api/v1/auth/allusers`;
+export const getAllUsersUrl = `${backendURL}/api/v1/auth/allusers/`;
 export const addMessageUrl = `${backendURL}/api/v1/chat/addmessage`;
 export const getMessagesUrl = `${backendURL}/api/v1/chat/getmessages`;
